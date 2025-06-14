c64334c5115211f6663af7bda1f7e2894f5a8f3e
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-06-13 23:59:56 -0600

    Import all images

---
+++ frontend/src/components/SectionOne.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <HeaderPage />
+  </div>
+
+  <div class="body font container my-5">
+    <div class="row justify-content-center text-center">
+      <div class="col-auto">
+        <h1 class="font" style="color: #7b79a1;">Section: Who am I?</h1>
+        <h1 class="font hover-zoom" style="color: #b3596a;">
+          What is your name?</h1>
+        <img src="../assets/images/Section1/Mirror.png"
+            alt="Mirror"
+            class="img-fluid hover-zoom"
+            style="max-width: 300px; margin-top: 20px; margin-bottom: 20px"
+            @click="popClickSound"/>
+      </div>
+    </div>
+  </div>
+
+  <CreditsFooter />
+</template>
+
+<script>
+import HeaderPage from "./Header.vue";
+import CreditsFooter from "./Credits.vue";
+export default {
+  components: {
+    HeaderPage,
+    CreditsFooter
+  },
+};
+</script>
