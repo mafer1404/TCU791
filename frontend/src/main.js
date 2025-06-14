f9d6a620b4ca7cfb0d1cd00a02c414bf253d44da
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-06-14 00:01:38 -0600

    Add vuerouter paths

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,33 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from "./components/HomePage.vue";
+import SectionOne from "./components/SectionOne.vue";
+import SectionTwo from "./components/SectionTwo.vue";
+import SectionThreePartOne from "./components/SectionThreePartOne.vue";
+import SectionThreePartTwo from "./components/SectionThreePartTwo.vue";
+import SectionFour from "./components/SectionFour.vue";
+
+import "./assets/css/FontBody.css"
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "HomePage", component: HomePage },
+    { path: "/SectionOne", name: "SectionOne", component: SectionOne },
+    { path: "/SectionTwo", name: "SectionTwo", component: SectionTwo },
+    { path: "/SectionThreePartOne", name: "SectionThreePartOne"
+      , component: SectionThreePartOne },
+    { path: "/SectionThreePartTwo", name: "SectionThreePartTwo"
+      , component: SectionThreePartTwo },
+    { path: "/SectionFour", name: "SectionFour"
+      , component: SectionFour }
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
